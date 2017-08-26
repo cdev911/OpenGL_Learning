d5ca9484b13fe98d9d00c4cf321d79d2d3c942dd
cdev911 <cdev911@gmail.com>
2017-08-27 00:06:26 +0200

    Setting up, first commit

---
+++ OpenGLLearn/src/OpenGLLearn.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OpenGLLearn.cpp
+// Author      : Chaitanya
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "OpeGL Learning" << endl; // prints OpeGL Learning
+	return 0;
+}
