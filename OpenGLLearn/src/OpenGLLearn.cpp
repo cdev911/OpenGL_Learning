540cfe15369fb76b7ba78ce65b51693b0febc84c
cdev911 <cdev911@gmail.com>
2017-08-27 00:28:29 +0200

    For loop added

--- OpenGLLearn/src/OpenGLLearn.cpp
+++ OpenGLLearn/src/OpenGLLearn.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 int main(int argc , char *argv[]) {
 	cout << "OpeGL Learning" << endl; // prints OpeGL Learning
-
+	int sum=0;
+	for (int i = 0 ; i < 10 ; ++i)
+		sum+=i;
 	return 0;
 }
